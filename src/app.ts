d461ed842a77d1338fca8872813174becefc792d
adnan-akhlas <mail.adnanakhlas@gmail.com>
2026-03-14 02:21:09 +0600

    feat(auth): set token to cookie

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express, { Application, Request, Response } from "express";
 import cors from "cors";
+import cookieParser from "cookie-parser";
 import httpStatus from "http-status-codes";
 import globalRouter from "./routes";
 import globalErrorHandler from "./middlewares/globalError";
@@ -7,6 +8,7 @@ import globalErrorHandler from "./middlewares/globalError";
 const app: Application = express();
 
 app.use(express.json());
+app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use("/api/v1", globalRouter);
--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -3,10 +3,17 @@ import catchAsync from "../../utils/catchAsync";
 import sendResponse from "../../utils/sendResponse";
 import httpStatus from "http-status-codes";
 import * as authService from "./auth.service";
+import env from "../../config/env";
 
 const loginUser = catchAsync(async (req: Request, res: Response) => {
   const body = req.body;
   const data = await authService.loginUser(body);
+
+  res.cookie("access_token", data, {
+    httpOnly: true,
+    secure: env.NODE_ENV === "production",
+  });
+
   sendResponse(res, {
     status: httpStatus.OK,
     success: true,
